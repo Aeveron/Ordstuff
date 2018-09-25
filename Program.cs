360552c5b73fc4acaf83cc4f2bd1f3a852308a0b
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-09-25 09:59:47 +0200

    x
    x

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace OrdStuff
         {
             var allLines = File.ReadAllLines(@"ordliste.txt");
             var lastWord = string.Empty;
-            var words = WordListArray(allLines, lastWord);             
+            var words = WordListArray(allLines, lastWord);
             var randomWordIndex = Random.Next(words.Length);
 
             var randomWord = words[randomWordIndex];
@@ -25,19 +25,14 @@ namespace OrdStuff
 
             while (wordCounter > 0)
             {
-                
+                var matchFound = FindRiddles(words);
+                if (matchFound) wordCounter--;
             }
 
-            var ending = randomWord.Substring(randomWord.Length -4);
-
-
-
+            var ending = randomWord.Substring(randomWord.Length - 4);
 
             Console.WriteLine(randomWord);
             Console.WriteLine(ending);
-
-            
-            
 
             //foreach (var word in words)
             //{
@@ -47,6 +42,22 @@ namespace OrdStuff
             //    Console.WriteLine(randomWord);
             //}
 
+        }
+
+        private static bool FindRiddles(string[] words)
+        {
+            var randomWordIndex = Random.Next(words.Length);
+            var selectedWord = words[randomWordIndex];
+            Console.Write(selectedWord + " - ");
+           
+            foreach (var word in words)
+            {
+                if (!CompareWordStartAndEnding(selectedWord, word)) continue;
+                Console.WriteLine(word);
+                return true;
+            }
+            Console.WriteLine("<fant ikke match>");
+            return false;
         }
 
         private static bool CompareWordStartAndEnding(string word1, string word2)
